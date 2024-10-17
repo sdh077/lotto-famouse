5f705e1b08bca8551cb94beff4f4fb5991665822
shindaeho <sdh077@naver.com>
2024-10-17 13:53:41 +0900

    skeleton

--- src/page/LatestLotto.tsx
+++ src/page/LatestLotto.tsx
@@ -4,7 +4,19 @@ import { lottoApi } from "@/app/api/lotto/route";
 import { PlusIcon } from "@radix-ui/react-icons";
 import { useQuery } from "@tanstack/react-query";
 import { useState } from "react";
+import { Skeleton } from "@/components/ui/skeleton"
 
+export function SkeletonView() {
+  return (
+    <div className="flex items-center space-x-4 h-40">
+      <Skeleton className="h-12 w-12 rounded-full" />
+      <div className="space-y-2">
+        <Skeleton className="h-4 w-[250px]" />
+        <Skeleton className="h-4 w-[200px]" />
+      </div>
+    </div>
+  )
+}
 
 export default function LatestLotto({ latestNo }: { latestNo: number }) {
   const [drwNo, setDrwNo] = useState(latestNo)
@@ -16,7 +28,7 @@ export default function LatestLotto({ latestNo }: { latestNo: number }) {
   const containerStyles = "flex gap-6"
   const iconStyles = "w-12 h-12 p-2 border border-accent rounded-full flex justify-center items-center text-accent text-base hover:bg-accent hover:text-primary hover:transition-all duration-500"
   const addStyles = "w-12 h-12 p-2 rounded-full flex justify-center items-center text-accent text-base "
-  if (query.isLoading) return <></>
+  if (query.isLoading) return <SkeletonView />
   return (
     <div className="relative border border-slate-800 backdrop-blur-xl flex flex-col items-center justify-center py-8 text-sm antialiased">
       <div className="flex justify-center mb-4 scroll-m-20 text-2xl font-bold tracking-tight text-black dark:text-white">
