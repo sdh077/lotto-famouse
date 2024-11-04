d5185a63fe051276ea75d71262ab341657cbdf6d
shindaeho <sdh077@naver.com>
2024-11-04 14:31:41 +0900

    ㅕㅔ

--- src/app/(main)/page.tsx
+++ src/app/(main)/page.tsx
@@ -1,17 +1,25 @@
-import { NEXT_PUBLIC_API_URL } from "@/lib/constants";
 import BestShop from "@/page/BestShop";
 import LatestLotto from "@/page/LatestLotto";
 import LocationInfo from "@/page/LocationInfo";
+import axios from 'axios';
+import * as cheerio from 'cheerio';
+
 export const revalidate = 3600 // revalidate at most every hour
 
 const getData = async () => {
-  return await fetch(`${NEXT_PUBLIC_API_URL}/api/init`).then(res => {
-    return res.json()
-  }).catch(() => 1140)
+  const url = 'https://dhlottery.co.kr/gameResult.do?method=byWin';
+  const { data } = await axios.get(url);
+
+  // cheerio를 사용해 HTML 파싱
+  const $ = cheerio.load(data);
+
+  // selected 속성이 있는 option 찾기
+  const latestNo = $('#dwrNoList option[selected]').val();
+  return Number(latestNo)
 }
 
 export default async function Home() {
-  const { latestNo } = await getData()
+  const latestNo = await getData()
   return (
     <div className="relative grid grid-cols-1 md:grid-cols-2 gap-4 container pt-8 h-full">
       <div className='flex flex-col gap-4'>
